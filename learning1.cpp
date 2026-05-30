1ef882ee35b12c7e435c5570bfb24c62525ba24a
Shubham88lodhi <shubhamkumarlodhi88@gmail.com>
2026-05-30 14:23:32 +0530

    Add files via upload

---
+++ learning1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    cout<<"I am shubham lodhi a Future MNNIT allahbad student"  << endl ;
+
+    int a = 7677 ;
+
+    // only use single quotes in (char) function and only single letter  because it is only of 1 Byte .
+    char ch = 'S' ;
+
+    bool bl = true ;
+
+    float fl = 1.3;
+
+    int dl  = 1.23 ;
+    int size = sizeof(a);
+
+    int c = 'A' ;
+
+
+    cout << size << endl ;
+    cout << a << '\n'  ;
+    cout << ch << endl ;
+    cout << bl << '\n'  ;
+    cout << fl  << endl  ;
+    cout << dl << endl ;
+    cout << c << '\n' ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
